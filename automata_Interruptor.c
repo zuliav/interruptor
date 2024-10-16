90d0dafc996b8a4bcb3d530338b1e6a06b91da4f
Zuli Avendaño <zulianiavendano920@gmail.com>
2024-10-16 10:01:36 -0600

    first commit

---
+++ automata_Interruptor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int on(int interruptor);
+int off(int interruptor);
+
+int main(){
+	int interruptor;
+	printf("apagado\n");
+	scanf("\n%i", &interruptor);
+	while(interruptor==0){
+		on(interruptor);
+		printf("encendido\n");
+		scanf("\n%i", &interruptor);
+		if(interruptor==0){
+			off(interruptor);
+			printf("apagado\n");
+		}
+		scanf("%i", &interruptor);
+	}
+	return 0;
+}
+
+int on(int interruptor){
+	return 0;
+}
+
+int off(int interruptor){
+	return 0;	
+}
+
